8bc90fe8246445ed98d2a7a3fab32f884be47765
Zhao.Wu <wuzhao-1@thunisoft.com>
2021-03-26 20:35:18 +0800

    feature 增加支付订实体

--- src/main/java/studio/greeks/progcs/entity/PayOrder.java
+++ src/main/java/studio/greeks/progcs/entity/PayOrder.java
@@ -1,9 +1,51 @@
 package studio.greeks.progcs.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
 /**
  * 支付订单信息
  *
  * @author <a href="mailto:wuzhao-1@thunisoft.com>吴昭</a>
  */
-public class PayOrder {
+@Entity
+@Table(name = "t_pay_order",
+        indexes = {
+            @Index(name = "i_pay_order_member_id", columnList = "member_id")
+        }
+)
+public class PayOrder extends BaseEntity {
+    /**
+     * 订单名称
+     */
+    private String name;
+
+    /**
+     * 订单类型
+     */
+    private Integer type;
+
+    /**
+     * 订单总金额
+     */
+    private BigDecimal totalAmount;
+
+    /**
+     * 支付状态
+     */
+    private Integer status;
+
+    /**
+     * 支付订单编号
+     */
+    private String payOrderNo;
+
+    /**
+     * 所属
+     */
+    @Column(name = "member_id", nullable = false)
+    private String memberId;
 }
